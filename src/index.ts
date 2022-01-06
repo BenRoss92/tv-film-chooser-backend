b7ae2e15f84eb843ebdd00d1218bb2da0b654d5e
Ben Ross <benjamin.ross@youview.com>
2022-01-06 16:34:23 +0000

    Get all entities with titles from endpoint (no database)

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,31 @@ app.get('/', (req, res) => {
     res.send("Aaaaaaaah you found me!!!")
 });
 
+app.get('/title-entities', (req, res) => {
+    res.send([
+        {
+          id: 1,
+          title: 'Terminator 1',
+        },
+        {
+          id: 2,
+          title: 'Terminator 2',
+        },
+        {
+          id: 3,
+          title: 'Terminator 3',
+        },
+        {
+          id: 4,
+          title: 'Terminator 4',
+        },
+        {
+          id: 5,
+          title: 'Terminator 5',
+        },
+      ]);
+});
+
 app.listen(port, () => {
     console.log(`server started at http://localhost:${port}`);
-})
+});
